14e28d4d4e34f97675a6394415ff95feb27d82cf
Daniel Richter <daniel.richter@msab.com>
2024-12-17 16:39:09 +0100

    fixed startup

--- ICA.API/Program.cs
+++ ICA.API/Program.cs
@@ -1,11 +1,19 @@
 using ICA.Application;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<IcaDbContext>();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<IcaDbContext>(options => options.UseInMemoryDatabase("TestDatabase"));
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<IcaDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+app.MapControllers();
 
 app.Run();
